131bf2919feea28d194a8e363f41ef8b6e927974
cnboker <coobots@gmail.com>
2025-11-15 11:11:40 +0800

    success

--- src/my_cosyvoice.py
+++ src/my_cosyvoice.py
@@ -1,15 +1,71 @@
-import cosyvoice
-
-import soundfile as sf
+from pathlib import Path
+import sys
 import os
-from src.sentences_chunk import sentence_splitting
+import shutil
+from pydub import AudioSegment
+from glob import glob
 
-tts = cosyvoice.load_model("cosyvoice-tts-zh")
-os.makedirs("audio", exist_ok=True)
+# 必须在 import 之前
+sys.path.append("external/CosyVoice")
+sys.path.append('external/CosyVoice/third_party/Matcha-TTS')
 
+from cosyvoice.cli.cosyvoice import CosyVoice, CosyVoice2
+from cosyvoice.utils.file_utils import load_wav
+import torchaudio
+
+
+
+cosyvoice = None
+prompt_speech_16k = None
+os.makedirs('audio',exist_ok=True)
+os.makedirs("assets/audio_parts", exist_ok=True)
+
+def init_model():
+    global cosyvoice
+    global prompt_speech_16k
+    cosyvoice = CosyVoice2('external/CosyVoice/pretrained_models/CosyVoice2-0.5B', load_jit=False, load_trt=False, fp16=False)    
+    prompt_speech_16k = load_wav('external/CosyVoice/asset/zero_shot_prompt.wav', 16000)
 
 def cos_voice(file:str):
-    chunks = sentence_splitting(file=file)
-    for i, sentence in enumerate(chunks):
-        audio = tts.tts(sentence)
-        sf.write(f"audio/part_{i:05d}.wav", audio, 44100)
+    #clear dir
+    clear_dir('audio')
+
+    global cosyvoice
+    global prompt_speech_16k
+
+    if cos_voice is None or prompt_speech_16k is None:
+        init_model()
+
+    with open(file, 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    for i, j in enumerate(cosyvoice.inference_cross_lingual(text, prompt_speech_16k, stream=False)):
+        torchaudio.save('audio/part_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
+    out_file = Path(file).stem + '.wav'
+    print('outfile->', out_file)
+    audio_merge(f'assets/audio_parts/{out_file}')
+
+def audio_merge(out_file:str):
+    files = sorted(glob("audio/part_*.wav"))
+    final = AudioSegment.empty()
+
+    for f in files:
+        final += AudioSegment.from_wav(f) + AudioSegment.silent(300)  # 句子之间留 0.3 秒
+
+    final.export(out_file, format="wav", bitrate="192k")
+    
+def clear_dir(path):
+    if not os.path.isdir(path):
+        print(f"{path} is not a directory.")
+        return
+
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.remove(file_path)          # 删除文件
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)      # 删除子目录
+        except Exception as e:
+            print(f"Failed to delete {file_path}: {e}")
+
--- test2.py
+++
@@ -1,5 +0,0 @@
-from cosyvoice import CosyVoice
-
-model = CosyVoice("cosyvoice-tts")
-audio = model.tts("你好，我是 CosyVoice 模型。")
-model.save(audio, "demo.wav")
